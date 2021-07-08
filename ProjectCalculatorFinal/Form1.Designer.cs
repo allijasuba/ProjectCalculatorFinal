b21e199aadf567ba31aaa307c30e771f74e2670a
allijasuba <allijasuba@gmail.com>
2021-07-08 16:10:27 +0800

    added textbox

--- ProjectCalculatorFinal/Form1.Designer.cs
+++ ProjectCalculatorFinal/Form1.Designer.cs
@@ -29,20 +29,40 @@ namespace ProjectCalculatorFinal
         /// </summary>
         private void InitializeComponent()
         {
+            this.result = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(28, 30);
+            this.result.Name = "result";
+            this.result.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.result.Size = new System.Drawing.Size(367, 50);
+            this.result.TabIndex = 0;
+            this.result.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(422, 496);
+            this.ClientSize = new System.Drawing.Size(424, 496);
+            this.Controls.Add(this.result);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(440, 535);
+            this.MinimumSize = new System.Drawing.Size(440, 535);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.Text = "Calculator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox result;
     }
 }
 
